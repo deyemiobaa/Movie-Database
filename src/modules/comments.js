de4826c993f27569907b080ebc0dff8843e325d0
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-13 11:51:02 +0100

    Write functios to add comments

---
+++ src/modules/comments.js
@@ -0,0 +1,37 @@
+import { addComments, getComments } from './getvshow.js';
+
+const form = document.getElementById('form');
+const username = document.getElementById('name');
+const comment = document.getElementById('comment');
+const commentCount = document.querySelector('.comment-count');
+const commentList = document.querySelector('.comment-list');
+
+const getAllComments = async (id) => {
+  try {
+    const allComments = await getComments(id);
+    commentCount.textContent = `Comments (${allComments.length})`;
+    commentList.innerHTML = '';
+    allComments.forEach((e) => {
+      commentList.innerHTML += `
+    <li>${e.creation_date}  ${e.username}:  ${e.comment}</li>
+    `;
+    });
+  } catch (error) {
+    const allComments = [];
+    commentCount.textContent = `Comments (${allComments.length})`;
+  }
+};
+
+const postAComment = () => {
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const movieId = form.parentNode.id;
+    const response = await addComments(movieId, username.value, comment.value);
+    if (response === 201) {
+      await getAllComments(movieId);
+    }
+    form.reset();
+  });
+};
+
+export { postAComment, getAllComments };
