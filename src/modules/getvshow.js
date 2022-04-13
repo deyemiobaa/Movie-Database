3b72da9534b031bd19231dcc25cc37c07db245f8
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-13 11:51:17 +0100

    fetch comments from API

--- src/modules/getvshow.js
+++ src/modules/getvshow.js
@@ -2,4 +2,24 @@ const getvShow = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   return response.json();
 };
-export default getvShow;
+
+const addComments = async (itemId, username, comment) => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ED1rQK1snOoAMYBCYLLf/comments', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      comment,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response.status;
+};
+
+const getComments = async (id) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ED1rQK1snOoAMYBCYLLf/comments?item_id=${id}`);
+  return response.json();
+};
+export { getvShow, addComments, getComments };
