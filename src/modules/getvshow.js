d2add7a04f503aa218e0afa1f46af8e7a4f186c4
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-13 14:04:47 +0300

    like count and display

--- src/modules/getvshow.js
+++ src/modules/getvshow.js
@@ -1,5 +1,26 @@
+/* eslint-disable import/no-cycle */
+import renderShows from './render.js';
+
 const getvShow = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   return response.json();
 };
-export default getvShow;
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ED1rQK1snOoAMYBCYLLf/likes';
+const addLikes = async (id) => {
+  await fetch(url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  renderShows();
+};
+const getlikes = (async () => {
+  const response = await fetch(url, { method: 'GET' });
+  return response.json();
+});
+export { getvShow, addLikes, getlikes };
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,8 +1,16 @@
-import getvShow from './getvshow.js';
+/* eslint-disable import/no-cycle */
+import { getvShow, addLikes, getlikes } from './getvshow.js';
 import selector from './selectors.js';
 import Popup from './popup.js';
 
 const clickEvents = () => {
+  const likeBtn = document.querySelectorAll('.like-btn');
+  likeBtn.forEach((e) => {
+    e.addEventListener('click', () => {
+      const { id } = e.parentNode.parentNode;
+      addLikes(id);
+    });
+  });
   const popup = new Popup();
   const showList = document.querySelectorAll('.fa-comment');
   showList.forEach((e) => {
@@ -18,18 +26,26 @@ const clickEvents = () => {
 const renderShows = (async () => {
   let render;
   const shows = await getvShow();
+  const likes = await getlikes();
+  let likeCount = 0;
   shows.forEach((e) => {
+    likes.forEach((eachLikes) => {
+      if (String(eachLikes.item_id) === String(e.id)) {
+        likeCount = eachLikes.likes;
+      }
+    });
     render += `
     <li class="show"  id=${e.id}><img src=${e.image.medium} alt="movie-poster" class="movie-poster">
     <h3 class="show-name">${e.name}</h3>
     <div class="show-btns">
-    <i class="fa-solid fa-heart">
+    <i class="fa-solid fa-heart like-btn">
     </i>
-    <p></p>
+    <p> ${likeCount}</p>
     <i class="fa-solid fa-comment"></i>
     </div>
     </li>
     `;
+    likeCount = 0;
   });
 
   selector.allShows.innerHTML = render.replace('undefined', '');
