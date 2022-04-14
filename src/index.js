1bd0b0516adf7d0f06595bee2b588c65ec09a169
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-14 19:40:09 +0100

    Record user session

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import poster from './modules/wallpaper-dk.svg';
 import renderShows from './modules/render.js';
 import { postAComment } from './modules/comments.js';
 import delayDisplay from './modules/animations.js';
+import selector from './modules/selectors.js';
 
 document.querySelector('.poster').src = poster;
 
-delayDisplay();
+const sessionstore = sessionStorage.getItem('session') || false;
+if (sessionstore === false) {
+  delayDisplay();
+  sessionStorage.setItem('session', 'true');
+} else {
+  selector.overlay.style.display = 'none';
+}
+
 renderShows();
 postAComment();
