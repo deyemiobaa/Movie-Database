1e5b86dc46e4c4bab289f404bbd175e7c2715bdc
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-15 03:29:05 +0100

    Add welcome popup to mobile

--- src/modules/animations.js
+++ src/modules/animations.js
@@ -5,14 +5,11 @@ const delayDisplay = () => {
   selector.headerWrap.style.display = 'none';
   document.body.classList.add('scroll-hide');
 
-  setTimeout(() => {
-    selector.allShows.style.display = 'grid';
-    selector.headerWrap.style.display = 'flex';
-  }, 2000);
-
   selector.button.addEventListener('click', () => {
     selector.overlay.style.display = 'none';
     document.body.classList.remove('scroll-hide');
+    selector.allShows.style.display = 'grid';
+    selector.headerWrap.style.display = 'flex';
   });
 };
 
